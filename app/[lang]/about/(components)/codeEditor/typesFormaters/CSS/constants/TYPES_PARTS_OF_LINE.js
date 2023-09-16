f66e753a4b707ce0965008a8bb54b2d50f538b8d
LeoAban <83424492+LeoAban@users.noreply.github.com>
2023-09-16 00:31:03 -0300

    Delete app/[lang] directory

--- app/[lang]/about/(components)/codeEditor/typesFormaters/CSS/constants/TYPES_PARTS_OF_LINE.js
+++
@@ -1,9 +0,0 @@
-export const TYPES_PARTS_OF_LINE = {
-  keyword: 'keyword',
-  class: 'class',
-  property: 'property',
-  value: 'value',
-  operator: 'operator',
-  tag: 'tag',
-  error: 'error'
-}

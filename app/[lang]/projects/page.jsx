b78cb26e6061361d93bde5464440637fc68f6125
LeoAban <83424492+LeoAban@users.noreply.github.com>
2023-09-17 20:14:06 -0300

    Update page.jsx

--- app/[lang]/projects/page.jsx
+++ app/[lang]/projects/page.jsx
@@ -7,8 +7,7 @@ import dictionary from '@/content'
 
 const VideoYoutube = () => {
   return (
-    <iframe src='https://www.youtube.com/embed/VIDEO_ID' frameborder='0' allowfullscreen />
-
+        <iframe style={{ width: '100%' }} height='315' src='https://www.youtube.com/embed/JvGFWqf7DRc?si=mNz-MGuJcZWbKdds' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen />
   )
 }
 
